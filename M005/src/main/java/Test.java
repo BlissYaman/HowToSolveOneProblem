90c4f0ac21a551ff79d61874f99603819ee9dcfe
BlissYaman <128260393+BlissYaman@users.noreply.github.com>
2025-02-17 11:02:15 +0100

    Update Test.java

--- M005/src/main/java/Test.java
+++ M005/src/main/java/Test.java
@@ -2,10 +2,12 @@ public class Test {
 
     public static void main(String[] args) {
         String result="Total is 45";
-        //I want to increase 5 total
+        //I want to increase 5 total (The String should be "Total is 50")
 
         String newResult= result.substring(0,result.lastIndexOf(" "))+(Integer.parseInt(result.substring(result.lastIndexOf(" ")))+5);
-
+        // result.substring(0,result.lastIndexOf(" "))= Total is 
+        // Integer.parseInt(result.substring(result.lastIndexOf(" ")))=45
+        // result.substring(result.lastIndexOf(" "))="45"
         System.out.println(newResult);
 
     }
