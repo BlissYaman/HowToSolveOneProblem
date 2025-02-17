1dfb2b8ee27513b24be53b91ce274e1a8b77f6df
BlissYaman <128260393+BlissYaman@users.noreply.github.com>
2025-02-17 10:41:58 +0100

    Update Test.java

--- M004/src/main/java/Test.java
+++ M004/src/main/java/Test.java
@@ -1,8 +1,9 @@
 public class Test {
-
+    // name=Java
     static String name;
     public static void main(String[] args) {
 
-        System.out.println(name.substring(0,1));
+        System.out.println(name.substring(0,1)); // J
+        // System.out.println(name.substring(0,4)/name.length()); Java
     }
 }
